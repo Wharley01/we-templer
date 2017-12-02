5d62abac53434342f64dabbbaf8f3aa6dfe8aafd
Wharley01 <hackerslord96@gmail.com>
2017-12-02 15:34:34 +0100

    Starting all over again

---
+++ src/Templer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP ENVY
+ * Date: 02/12/2017
+ * Time: 14:57
+ */
+
+namespace We;
+
+
+class Templer
+{
+
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP ENVY
+ * Date: 02/12/2017
+ * Time: 15:33
+ */
