df3d107b254418180a2d02fe1e04d1664c71fb89
Wharley01 <hackerslord96@gmail.com>
2017-12-03 23:43:59 +0100

    Variable Assignation and usage in template just worked fine

--- src/Templer.php
+++ src/Templer.php
@@ -39,26 +39,55 @@ class Templer
         return $this;
     }
     public function Render(){
-        $this->interpret_vars();
+        $this->replace_all_vars();
         echo $this->file_interpreted;
     }
-    public function interpret_vars(){
+    public function interpret_vars($raw){
         $rule = "{$this->open_tag}\s*{$this->var_sym}{$this->var_rule}{$this->close_tag}";
-        $this->file_interpreted = preg_replace_callback("/{$rule}/",function($m){
+        $file_interpreted = $raw;
+        $file_interpreted = preg_replace_callback("/{$rule}/",function($m){
             $var = @$this->data[$m[1]];
             if (preg_match("/\./",$m[1])){
                 $break_w = explode(".",$m[1]);
                 $root = $this->data[$break_w[0]];
                 for($i = 1;$i < count($break_w);$i++){
+                    if(!@$root[$break_w[$i]]){
+                        throw new \Exception("Error: Undefined index '{$break_w[$i]}' in '{$break_w[$i-1]}'");
+                    }
                     $root = $root[$break_w[$i]];
                 }
+
                 return $root;
             }elseif(!$var){
                 throw new \Exception("Error: Undefined variable name \"{$m[0]}\"");
             }else{
+
               return $var;
             }
+        },$file_interpreted);
+
+        return $file_interpreted;
+    }
+
+    public function assign_vars(){
+        $rule = "{$this->open_tag}\s*{$this->var_sym}{$this->var_rule}\s*=\s*([^]]*)\s*{$this->close_tag}";
+
+        $this->file_interpreted = preg_replace_callback("/{$rule}/",function($m){
+            $rule = "\`{$this->var_sym}{$this->var_rule}\`";
+            $content = $m[2];
+            $content = preg_replace_callback("/{$rule}/",function($r){
+
+                $return = "{$this->open_tag}{$this->var_sym}{$r[1]}{$this->close_tag}";
+                return str_replace("\\","",$return);
+            },$content);//convert in-text variable to real one
+            $interpret = $this->interpret_vars($content);
+            $this->data[$m[1]] = $interpret;
+            return "";
         },$this->file_interpreted);
+    }
+    public function replace_all_vars(){
+        $this->assign_vars();
+        $this->file_interpreted = $this->interpret_vars($this->file_interpreted);
     }
     public function interpret_conditional_statement(){
         return $this->file_interpreted;
