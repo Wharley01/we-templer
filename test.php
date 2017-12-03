05f03be68c606a9bcbab882524be4df174e35c6b
Wharley01 <hackerslord96@gmail.com>
2017-12-03 23:44:50 +0100

    More test cases added

--- test.php
+++ test.php
@@ -4,6 +4,9 @@ use We\Templer;
 
 try{
     $test = new Templer("template.wtp");
+    $test->bind(["body" => ["text" => "This is the body text"],"title" => "This is the page title!"]);
+
+
     $test->Render();
 }catch (Exception $e){
     echo $e->getMessage();
